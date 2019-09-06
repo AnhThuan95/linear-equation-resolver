218e52bef2ebe947fa2a3938fac13163c5f83cff
Thuan Phong <AnhThuan95>
2019-09-06 10:25:34 +0700

    add

---
+++ src/main/java/LinearEquationResolver.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as \"a * x + b = 0\", please enter constants:");
+
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+
+        if (a != 0) {
+            double solution = -b/a;
+            System.out.printf("The solution is: %f!", solution);
+        } else {
+            if (b == 0) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+    }
+}
